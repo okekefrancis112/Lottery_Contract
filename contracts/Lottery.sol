aa1882d043efe95c88ba5323c370b3925bce48e5
okekefrancis112 <okeke98@gmail.com>
2022-08-27 02:41:11 +0100

    update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.16;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -9,6 +9,7 @@ contract Lottery is VRFConsumerBase, Ownable {
     address payable public recentWinner;
     uint256 public randomness;
     uint256 public usdEntryFee;
+    string public lotteryName;
 
     enum LOTTERY_STATE {
         OPEN,
@@ -23,7 +24,6 @@ contract Lottery is VRFConsumerBase, Ownable {
 
     event RequestedRandomness(bytes32 requestId);
     
-
     constructor(address _vrfCoordinator, address _link, uint256 _fee, bytes32 _keyhash) VRFConsumerBase(_vrfCoordinator, _link) {
         entryFee = 1 ether;
         lottery_state = LOTTERY_STATE.CLOSED;
@@ -31,21 +31,24 @@ contract Lottery is VRFConsumerBase, Ownable {
         keyhash = _keyhash;
     }
 
-    function startLottery() public onlyOwner {
+    function startLottery(string memory _name) public onlyOwner {
         require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
         lottery_state = LOTTERY_STATE.OPEN;
+        lotteryName = _name;
     }
 
     function joinLottery() payable public {
+        require(msg.sender != address(0), "Not a valid Address");
         require(lottery_state == LOTTERY_STATE.OPEN);
         require(msg.value == 2 ether, "Not enough ETH");
         players.push(payable(msg.sender));
     }
 
-    function endLottery() public onlyOwner {
+    function endLottery() public onlyOwner returns(bytes32 _requestId) {
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
         bytes32 requestId = requestRandomness(keyhash, fee);
         emit RequestedRandomness(requestId);
+        return requestId;
     }
 
     function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
@@ -55,15 +58,23 @@ contract Lottery is VRFConsumerBase, Ownable {
         uint256 indexOfWinner = _randomness % players.length;
         recentWinner = players[indexOfWinner];
         payable(recentWinner).transfer(transferAmount()); 
+
         // Reset
         players = new address payable[](0);
         lottery_state = LOTTERY_STATE.CLOSED;
         randomness = _randomness;
-
     }
 
     function transferAmount() public view returns(uint tranferrableAmount) {
         uint amount = address(this).balance;
         tranferrableAmount = (amount * 10) / 100;
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns(address payable[] memory){
+        return players;
     }
 }
